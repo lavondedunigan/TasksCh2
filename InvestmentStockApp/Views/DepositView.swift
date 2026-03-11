706b6527a3bf2dbf922dffc97447ba661a02120b
Lavonde Dunigan <lavondedunigan05@gmail.com>
2026-03-11 16:04:09 -0500

    commit 3/11/2026

---
+++ InvestmentStockApp/Views/DepositView.swift
@@ -0,0 +1,18 @@
+//
+//  DepositView.swift
+//  InvestmentStockApp
+//
+//  Created by Lavonde Dunigan on 3/11/26.
+//
+
+import SwiftUI
+
+struct DepositView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DepositView()
+}
---
+++ InvestmentStockApp/Views/WithdrawView.swift
@@ -0,0 +1,18 @@
+//
+//  WithdrawView.swift
+//  InvestmentStockApp
+//
+//  Created by Lavonde Dunigan on 3/11/26.
+//
+
+import SwiftUI
+
+struct WithdrawView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WithdrawView()
+}
